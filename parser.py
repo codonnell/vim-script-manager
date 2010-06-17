5b50256d2a620865b6c44797cdc27d4a33f9cce3
Christopher O'Donnell <chris.bowdoin@gmail.com>
2010-06-17 17:56:56 -0400

    Trying to unpack vimballs. Other archive types working. Standalone .vim files not supported yet.

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ import re
 
 class VimScript(object):
     def __init__(self, name='', url='', version=1.0, uploadDate=date.today(),
-            vimVersion=7.2, author = '', description = ''):
+            vimVersion=7.2, author = '', description = '', files=[]):
         self.name = name
         self.url = url
         self.version = version
@@ -16,43 +16,56 @@ class VimScript(object):
         self.vimVersion = vimVersion
         self.author = author
         self.description = description
+        self.files = files
 
     def _download(self):
         """Downloads the vim script using its url attr.
         """
+        import os.path
+        from urllib import urlretrieve
         print self.url
         soup = BeautifulSoup(urlopen(self.url))
         downloadElt = soup.find(href=re.compile('download_script'))
-        downloadLink = '/'.join(['http://www.vim.org/scripts', downloadElt['href']])
+        downloadLink = os.path.join('http://www.vim.org/scripts', downloadElt['href'])
+        print downloadLink
         filepath = '/'.join(['/home/chris/Downloads', downloadElt.string])
-        from urllib import urlretrieve
         urlretrieve(downloadLink, filepath)
         return filepath
 
+    #TODO: Extract vimballs and extract to the correct directory.
+    #TODO: Correctly determine to which directory .vim files should be moved.
     def _unpack(self, filepath):
-        """Determines the vim script's extension and unpacks it.
+        """Determines the vim script's extension and unpacks it. Also sets the
+        files variable.
         """
         import os.path
         root, ext = os.path.splitext(filepath)
         if ext == u'.zip':
             from zipfile import ZipFile
             archive = ZipFile(filepath, 'r')
+            self.files = [member.filename for member in archive.infolist()]
             archive.extractall(root)
-        elif ext == u'.tar':
+        elif ext == u'.tar' or ext == u'.tgz' or ext == u'.bz2':
             import tarfile
             archive = tarfile.open(filepath)
+            self.files = [member.name for member in archive.getmembers() if
+                    member.isfile()]
             archive.extractall(root)
         elif ext == u'.gz' and os.path.splitext(root)[1] == u'.tar':
             import tarfile
             archive = tarfile.open(filepath)
+            self.files = [member.name for member in archive.getmembers() if
+                    member.isfile()]
             archive.extractall(os.path.splitext(root)[0])
+        elif ext == u'.vba':
+            from subprocess import Popen
 
     def install(self):
         """Download and unpack the script into the vim runtime directory.
         """
-        filepath = self._download()
-        self._unpack(filepath)
-        #self._unpack(u'/home/chris/Downloads/taglist_45.zip')
+        #filepath = self._download()
+        #self._unpack(filepath)
+        self._unpack(u'/home/chris/Downloads/taglist_45.tar')
         
 
 # TODO: Finish this parsing.
@@ -70,7 +83,7 @@ class VimScript(object):
 
 if __name__ == "__main__":
     taglist = VimScript(name='project',
-            url='http://www.vim.org/scripts/script.php?script_id=69')
+            url='http://www.vim.org/scripts/script.php?script_id=273')
     # soup = BeautifulSoup(urlopen(taglist.url))
     # downloadSpan = soup.findAll(name='span', attrs={'class' : 'txth2'})
     # downloadLink = soup.find(href=re.compile('download_script'))
@@ -83,4 +96,10 @@ if __name__ == "__main__":
     # taglist.date = date(*dateList)
     # print dateList
     # print taglist.date
-    taglist.install()
+
+    #taglist.install()
+    import subprocess
+    vim = subprocess.Popen('vim', stdin=subprocess.PIPE)
+    vim.communicate(':e test.txt')
+    vim.communicate(':w')
+    vim.terminate()
