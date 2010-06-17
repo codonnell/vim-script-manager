9c14dbbd21a706359f2bf8a75851a68817948aef
Christopher O'Donnell <chris@codonnel-desktop.(none)>
2010-06-17 00:25:58 -0400

    Correctly finds download table. Created VimScript class.

--- parser.py
+++ parser.py
@@ -3,27 +3,22 @@
 
 from BeautifulSoup import BeautifulSoup #HTML Parser
 from urllib2 import urlopen
+from datetime import date
 import re
 
-#class vimHTMLParser(HTMLParser):
-#    def __init__(self, url):
-#        HTMLParser.__init__(self)
-#        req = urlopen(url)
-#        self.feed(req.read())
-#
-#    def handle_starttag(self, tag, attrs):
-#        print "Encountered the beginning of a %s tag" % tag
-#
-#    def handle_endtag(self, tag):
-#        print "Encountered the end of a %s tag" % tag
-#
-#    def handle_comment(self, data):
-#        print "Encountered a comment: %s" % data
+class VimScript(object):
+    def __init__(self, name = '', version=1.0, date=date.today(),
+            vimVersion=7.2, author = '', description = ''):
+        self.name = name
+        self.version = version
+        self.date = date
+        self.vimVersion = vimVersion
+        self.author = author
+        self.description = description
 
 if __name__ == "__main__":
     page = urlopen('http://www.vim.org/scripts/script.php?script_id=273')
     soup = BeautifulSoup(page)
     downloadSpan = soup.findAll(name='span', attrs={'class' : 'txth2'})
-    downloadLinks = soup.findAll(attrs={'href' : re.compile('download')})
-    print downloadLinks
-#    vimHTMLParser('file:/home/chris/coding/vim-script-manager/real_test.html')
+    downloadLink = soup.find(href=re.compile('download_script'))
+    print downloadLink.parent
