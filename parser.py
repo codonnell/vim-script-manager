10bd8225df652c95b7c726ed4ddb7ea9a3d4c8db
Christopher O'Donnell <chris.bowdoin@gmail.com>
2010-06-21 14:27:58 -0400

    Finished parsing. Need to find a good structure for the program. The parsing in particular doesn't fit well with the rest of the code.

--- parser.py
+++ parser.py
@@ -3,38 +3,64 @@
 
 import vim
 
-# TODO: Finish this parsing.
-# def parseScriptPage(url):
-#     soup = BeautifulSoup(urlopen(url))
-#     downloadLink = soup.find(href=re.compile('download_script'))
-#     versionEntry = downloadLink.parent.nextSibling.nextSibling
-#     version = float(versionEntry.contents[0].string)
-#     dateEntry = versionEntry.nextSibling.nextSibling
-#     dateString = dateEntry.contents[0].string
-#     dateList = dateString.split('-')
-#     dateList = [int(x) for x in dateList]
-#     uploadDate = date(*dateList)
-#     return VimScript('script', url, version, uploadDate)
+def parseScriptPage(script):
+    import BeautifulSoup
+    import re
+    from urllib2 import urlopen
+    from datetime import date
+    soup = BeautifulSoup.BeautifulSoup(urlopen(url))
+    downloadLink = soup.find(href=re.compile('download_script'))
+    # Version
+    versionEntry = downloadLink.parent.nextSibling.nextSibling
+    script.version = float(versionEntry.contents[0].string)
+    # Date
+    dateEntry = versionEntry.nextSibling.nextSibling
+    dateString = dateEntry.contents[0].string
+    dateList = dateString.split('-')
+    dateList = [int(x) for x in dateList]
+    script.uploadDate = date(*dateList)
+    # Vim Version
+    vimEntry = dateEntry.nextSibling.nextSibling
+    script.vimVersion = float(vimEntry.string)
+    # Author
+    authorEntry = vimEntry.nextSibling.nextSibling
+    script.author = authorEntry.contents[0].contents[0].string
+    # Short Description
+    descriptionEntry = authorEntry.nextSibling.nextSibling
+    description = [string.strip() for string in descriptionEntry.contents if
+            isinstance(string, BeautifulSoup.NavigableString)]
+    script.description = u' '.join(description)
+    return script
+
+def parseScriptPage(url):
+    import BeautifulSoup
+    import re
+    from urllib2 import urlopen
+    from datetime import date
+    soup = BeautifulSoup.BeautifulSoup(urlopen(url))
+    downloadLink = soup.find(href=re.compile('download_script'))
+    # Version
+    versionEntry = downloadLink.parent.nextSibling.nextSibling
+    version = float(versionEntry.contents[0].string)
+    # Date
+    dateEntry = versionEntry.nextSibling.nextSibling
+    dateString = dateEntry.contents[0].string
+    dateList = dateString.split('-')
+    dateList = [int(x) for x in dateList]
+    uploadDate = date(*dateList)
+    # Vim Version
+    vimEntry = dateEntry.nextSibling.nextSibling
+    vimVersion = float(vimEntry.string)
+    # Author
+    authorEntry = vimEntry.nextSibling.nextSibling
+    author = authorEntry.contents[0].contents[0].string
+    # Short Description
+    descriptionEntry = authorEntry.nextSibling.nextSibling
+    description = [string.strip() for string in descriptionEntry.contents if
+            isinstance(string, BeautifulSoup.NavigableString)]
+    descriptionString = u' '.join(description)
+    return VimScript('script', url, version, uploadDate, vimVersion, author,
+            descriptionString)
 
 if __name__ == "__main__":
-    taglist = VimScript(name='project',
-            url='http://www.vim.org/scripts/script.php?script_id=1318')
-    # soup = BeautifulSoup(urlopen(taglist.url))
-    # downloadSpan = soup.findAll(name='span', attrs={'class' : 'txth2'})
-    # downloadLink = soup.find(href=re.compile('download_script'))
-    # versionEntry = downloadLink.parent.nextSibling.nextSibling
-    # taglist.version = versionEntry.contents[0].string
-    # dateEntry = versionEntry.nextSibling.nextSibling
-    # dateString = dateEntry.contents[0].string
-    # dateList = dateString.split('-')
-    # dateList = [int(x) for x in dateList]
-    # taglist.date = date(*dateList)
-    # print dateList
-    # print taglist.date
-
-    taglist.install()
-    # import subprocess
-    # vim = subprocess.Popen(['vim', '-S #FILENAMEHERE'])# :q<cr>'])
-    # vim.communicate(':e test.txt')
-    # vim.communicate(':w')
-    # vim.terminate()
+    parseScriptPage('http://www.vim.org/scripts/script.php?script_id=273')
